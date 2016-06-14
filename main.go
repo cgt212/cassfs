868a3ac696aa61d0a36ebf2025984ffad4d0954c
Chris Tsonis <cgt212@whatbroke.com>
2016-06-14 01:18:40 +0000

    Added license information to all files

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+CassFs is a filesystem that uses Cassandra as the data store.  It is
+meant for docker like systems that require a lightweight filesystem
+that can be distributed across many systems.
+Copyright (C) 2016  Chris Tsonis (cgt212@whatbroke.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import "flag"
